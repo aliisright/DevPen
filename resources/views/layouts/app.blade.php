fe07af48a40496642d84fbc4bb97e15764de0f6d
Aliisright <alihasan.me@me.com>
2018-02-17 22:33:52 +0100

    layout app / scss => fonts text sizes and color scheme

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,56 +11,45 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+    <!--Fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Cabin|Patua+One|Raleway" rel="stylesheet">
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+      <nav class="navbar">
+        <div class="site-navbar container d-flex justify-content-between align-items-center">
 
-                    </ul>
+          <a class="link" href="#"><h1 class="text-size-l font-patua bg-dark-grey m-0">DevPen</h1></a>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a class="nav-link" href="{{ route('login') }}">Login</a></li>
-                            <li><a class="nav-link" href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        Logout
-                                    </a>
+          <ul class="list list-inline m-0">
+            @guest
+              <a class="link" href="#"><li class="font-cabin list-inline-item bg-grey mx-2">Connexion</li></a>
+              <a class="link" href="#"><li class="font-cabin list-inline-item bg-grey mx-2">Inscription</li></a>
+            @else
+              <a class="link" href="#"><li class="font-cabin list-inline-item bg-grey mx-2">Le Stream</li></a>
+              <a class="link" href="#"><li class="font-cabin list-inline-item bg-grey mx-2">Mon compte</li></a>
+              <a class="link" href="#"><li class="font-cabin list-inline-item bg-grey mx-2">Profile</li></a>
+              <a class="link" href="#"><li class="font-cabin list-inline-item bg-grey mx-2">{{Auth::user()->nickname}}</li></a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+              <a class="link" href="{{ route('logout') }}"
+                     onclick="event.preventDefault();
+                                   document.getElementById('logout-form').submit();"><li class="font-cabin list-inline-item bg-grey mx-2">Logout
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form></li></a>
+            @endguest
+          </ul>
+        </div>
+      </nav>
+
+
+      <main class="py-4">
+          @yield('content')
+      </main>
     </div>
 
     <!-- Scripts -->
